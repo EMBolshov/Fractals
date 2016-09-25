e647c368c37a957dbc4831886f168093920e875e
EMBolshov <e.m.bolshov@gmail.com>
2016-09-25 16:11:03 +0300

    Fractals
    My C# project, made to demonstrate some mathematical self-similar sets.
    Perfect example of recursion

---
+++ bolsh/Form1.Designer.cs
@@ -0,0 +1,343 @@
+﻿namespace bolsh
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_Draw = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textbox_it = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.pictureBox1s = new System.Windows.Forms.PictureBox();
+            this.label1s = new System.Windows.Forms.Label();
+            this.textBox1s = new System.Windows.Forms.TextBox();
+            this.serp1 = new System.Windows.Forms.Button();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.textBox1k = new System.Windows.Forms.TextBox();
+            this.resultPBk = new System.Windows.Forms.PictureBox();
+            this.drawButtonk = new System.Windows.Forms.Button();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.pictureBox1h = new System.Windows.Forms.PictureBox();
+            this.button1h = new System.Windows.Forms.Button();
+            this.textBox1h = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1s)).BeginInit();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.resultPBk)).BeginInit();
+            this.tabPage4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1h)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(734, 677);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.btn_Draw);
+            this.tabPage1.Controls.Add(this.pictureBox1);
+            this.tabPage1.Controls.Add(this.textbox_it);
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPage1.Size = new System.Drawing.Size(726, 644);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Кривая Гильберта";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(6, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Итерации";
+            // 
+            // btn_Draw
+            // 
+            this.btn_Draw.Location = new System.Drawing.Point(7, 74);
+            this.btn_Draw.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn_Draw.Name = "btn_Draw";
+            this.btn_Draw.Size = new System.Drawing.Size(129, 40);
+            this.btn_Draw.TabIndex = 6;
+            this.btn_Draw.Text = "Рисовать";
+            this.btn_Draw.UseVisualStyleBackColor = true;
+            this.btn_Draw.Click += new System.EventHandler(this.btn_Draw_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(155, 50);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(550, 550);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textbox_it
+            // 
+            this.textbox_it.Location = new System.Drawing.Point(98, 20);
+            this.textbox_it.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textbox_it.Name = "textbox_it";
+            this.textbox_it.Size = new System.Drawing.Size(38, 26);
+            this.textbox_it.TabIndex = 4;
+            this.textbox_it.Text = "3";
+            this.textbox_it.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.pictureBox1s);
+            this.tabPage2.Controls.Add(this.label1s);
+            this.tabPage2.Controls.Add(this.textBox1s);
+            this.tabPage2.Controls.Add(this.serp1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPage2.Size = new System.Drawing.Size(726, 644);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Ковер Серпинского";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1s
+            // 
+            this.pictureBox1s.Location = new System.Drawing.Point(155, 50);
+            this.pictureBox1s.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox1s.Name = "pictureBox1s";
+            this.pictureBox1s.Size = new System.Drawing.Size(550, 550);
+            this.pictureBox1s.TabIndex = 7;
+            this.pictureBox1s.TabStop = false;
+            // 
+            // label1s
+            // 
+            this.label1s.AutoSize = true;
+            this.label1s.ForeColor = System.Drawing.Color.Black;
+            this.label1s.Location = new System.Drawing.Point(6, 20);
+            this.label1s.Name = "label1s";
+            this.label1s.Size = new System.Drawing.Size(83, 20);
+            this.label1s.TabIndex = 6;
+            this.label1s.Text = "Итерации";
+            // 
+            // textBox1s
+            // 
+            this.textBox1s.Location = new System.Drawing.Point(98, 20);
+            this.textBox1s.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1s.Name = "textBox1s";
+            this.textBox1s.Size = new System.Drawing.Size(38, 26);
+            this.textBox1s.TabIndex = 5;
+            this.textBox1s.Text = "4";
+            this.textBox1s.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // serp1
+            // 
+            this.serp1.Location = new System.Drawing.Point(7, 74);
+            this.serp1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.serp1.Name = "serp1";
+            this.serp1.Size = new System.Drawing.Size(129, 40);
+            this.serp1.TabIndex = 4;
+            this.serp1.Text = "Рисовать";
+            this.serp1.UseVisualStyleBackColor = true;
+            this.serp1.Click += new System.EventHandler(this.serp1_Click);
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.label2);
+            this.tabPage3.Controls.Add(this.textBox1k);
+            this.tabPage3.Controls.Add(this.resultPBk);
+            this.tabPage3.Controls.Add(this.drawButtonk);
+            this.tabPage3.Location = new System.Drawing.Point(4, 29);
+            this.tabPage3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tabPage3.Size = new System.Drawing.Size(726, 644);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Множество Кантора";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // textBox1k
+            // 
+            this.textBox1k.Location = new System.Drawing.Point(98, 20);
+            this.textBox1k.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1k.Name = "textBox1k";
+            this.textBox1k.Size = new System.Drawing.Size(38, 26);
+            this.textBox1k.TabIndex = 7;
+            this.textBox1k.Text = "4";
+            this.textBox1k.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // resultPBk
+            // 
+            this.resultPBk.BackColor = System.Drawing.Color.White;
+            this.resultPBk.Location = new System.Drawing.Point(155, 50);
+            this.resultPBk.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.resultPBk.Name = "resultPBk";
+            this.resultPBk.Size = new System.Drawing.Size(550, 550);
+            this.resultPBk.TabIndex = 6;
+            this.resultPBk.TabStop = false;
+            // 
+            // drawButtonk
+            // 
+            this.drawButtonk.Location = new System.Drawing.Point(7, 74);
+            this.drawButtonk.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.drawButtonk.Name = "drawButtonk";
+            this.drawButtonk.Size = new System.Drawing.Size(129, 40);
+            this.drawButtonk.TabIndex = 5;
+            this.drawButtonk.Text = "Рисовать";
+            this.drawButtonk.UseVisualStyleBackColor = true;
+            this.drawButtonk.Click += new System.EventHandler(this.drawButtonk_Click);
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.label3);
+            this.tabPage4.Controls.Add(this.pictureBox1h);
+            this.tabPage4.Controls.Add(this.button1h);
+            this.tabPage4.Controls.Add(this.textBox1h);
+            this.tabPage4.Location = new System.Drawing.Point(4, 29);
+            this.tabPage4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tabPage4.Size = new System.Drawing.Size(726, 644);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Снежинка Коха";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1h
+            // 
+            this.pictureBox1h.Location = new System.Drawing.Point(155, 50);
+            this.pictureBox1h.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox1h.Name = "pictureBox1h";
+            this.pictureBox1h.Size = new System.Drawing.Size(550, 550);
+            this.pictureBox1h.TabIndex = 5;
+            this.pictureBox1h.TabStop = false;
+            // 
+            // button1h
+            // 
+            this.button1h.Location = new System.Drawing.Point(7, 74);
+            this.button1h.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1h.Name = "button1h";
+            this.button1h.Size = new System.Drawing.Size(129, 40);
+            this.button1h.TabIndex = 4;
+            this.button1h.Text = "Рисовать";
+            this.button1h.UseVisualStyleBackColor = true;
+            this.button1h.Click += new System.EventHandler(this.button1h_Click);
+            // 
+            // textBox1h
+            // 
+            this.textBox1h.Location = new System.Drawing.Point(98, 20);
+            this.textBox1h.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1h.Name = "textBox1h";
+            this.textBox1h.Size = new System.Drawing.Size(38, 26);
+            this.textBox1h.TabIndex = 3;
+            this.textBox1h.Text = "4";
+            this.textBox1h.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Итерации";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Итерации";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 677);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Фракталы";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1s)).EndInit();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.resultPBk)).EndInit();
+            this.tabPage4.ResumeLayout(false);
+            this.tabPage4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1h)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_Draw;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textbox_it;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.PictureBox pictureBox1s;
+        private System.Windows.Forms.Label label1s;
+        private System.Windows.Forms.TextBox textBox1s;
+        private System.Windows.Forms.Button serp1;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TextBox textBox1k;
+        private System.Windows.Forms.PictureBox resultPBk;
+        private System.Windows.Forms.Button drawButtonk;
+        private System.Windows.Forms.PictureBox pictureBox1h;
+        private System.Windows.Forms.Button button1h;
+        private System.Windows.Forms.TextBox textBox1h;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ bolsh/Form1.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bolsh
+{
+
+    public partial class Form1 : Form
+    {
+        private float m_X, m_Y;
+        private Bitmap m_Bm;
+        private int it;
+        private Bitmap m_Bm1;
+        int n;
+        static Pen pen1, pen2;
+        private Bitmap m_Bm2;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void btn_Draw_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int depth = Convert.ToInt32(textbox_it.Text);
+
+                float total_length, start_x, start_y, start_length;  // параметры для рис
+
+                if (pictureBox1.ClientSize.Height < pictureBox1.ClientSize.Width) //размер рисунка
+                {
+                    total_length = (float)(0.9 * pictureBox1.ClientSize.Height);
+                }
+                else
+                {
+                    total_length = (float)(0.9 * pictureBox1.ClientSize.Width);
+                }
+
+                start_x = (pictureBox1.ClientSize.Width - total_length) / 2;
+                start_y = (pictureBox1.ClientSize.Height - total_length) / 2;
+
+                start_length = (float)(total_length / (Math.Pow(2, depth) - 1));
+
+                m_Bm = new Bitmap(pictureBox1.ClientSize.Width, pictureBox1.ClientSize.Height);
+                pictureBox1.Image = m_Bm;
+
+                using (Graphics gr = Graphics.FromImage(m_Bm))
+                {
+                    gr.Clear(pictureBox1.BackColor); //рисуем кривую
+                    m_X = (int)start_x;
+                    m_Y = (int)start_y;
+                    Hilbert(gr, depth, start_length, 0);
+                    gr.Dispose();
+                }
+
+                pictureBox1.Refresh(); //выводим рез-тат
+                this.Cursor = Cursors.Default;
+            }
+            catch (Exception ex)
+
+            { MessageBox.Show(ex.Message.ToString()); }
+
+        }
+        private void Hilbert(Graphics gr, int depth, float dx, float dy)   // Draw a Hilbert curve.
+        {
+            if (depth > 1) Hilbert(gr, depth - 1, dy, dx);
+            DrawRelative(gr, dx, dy);
+            if (depth > 1) Hilbert(gr, depth - 1, dx, dy);
+            DrawRelative(gr, dy, dx);
+            if (depth > 1) Hilbert(gr, depth - 1, dx, dy);
+            DrawRelative(gr, -dx, -dy);
+            if (depth > 1) Hilbert(gr, depth - 1, -dy, -dx);
+        }
+
+        // Draw the line (m_X, m_Y)-(m_X + dx, m_Y + dy) and
+        // update m_X = m_X + dx, m_Y = m_Y + dy.
+        private void DrawRelative(Graphics gr, float dx, float dy)
+        {
+            gr.DrawLine(Pens.Black, m_X, m_Y, m_X + dx, m_Y + dy);
+            m_X = m_X + dx;
+            m_Y = m_Y + dy;
+        }
+
+        /////////////////////////////////// SERPINSKIY ///////////////////////
+        private void serp1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                it = Convert.ToInt32(textBox1s.Text);
+
+
+
+                m_Bm1 = new Bitmap(pictureBox1s.ClientSize.Width, pictureBox1s.ClientSize.Height);
+                pictureBox1s.Image = m_Bm1;
+
+                using (Graphics gr = Graphics.FromImage(m_Bm1))
+                {
+                    gr.Clear(Color.Black);
+                    //   serp(80, 80, 400, 400, it, gr);
+                    serp(0, 0, pictureBox1s.ClientSize.Width, pictureBox1s.ClientSize.Height, it, gr);
+                    gr.Dispose();
+                }
+            }
+            catch (OutOfMemoryException ex)
+
+            { MessageBox.Show(ex.Message.ToString()); }
+
+                      catch (Exception ex)
+
+            { MessageBox.Show(ex.Message.ToString()); }
+        }
+               void serp(float x1, float y1, float x2, float y2, int n, Graphics g)
+        {
+            float x1n, y1n, x2n, y2n;
+            if (n > 0)
+            {
+                x1n = 2 * x1 / 3 + x2 / 3;
+                x2n = x1 / 3 + 2 * x2 / 3;
+                y1n = 2 * y1 / 3 + y2 / 3;
+                y2n = y1 / 3 + 2 * y2 / 3;
+                g.FillRectangle(new SolidBrush(Color.Red), x1n, y1n, x2n - x1n, y2n - y1n);
+                serp(x1, y1, x1n, y1n, n - 1, g);
+                serp(x1n, y1, x2n, y1n, n - 1, g);
+                serp(x2n, y1, x2, y1n, n - 1, g);
+                serp(x1, y1n, x1n, y2n, n - 1, g);
+                serp(x2n, y1n, x2, y2n, n - 1, g);
+                serp(x1, y2n, x1n, y2, n - 1, g);
+                serp(x1n, y2n, x2n, y2, n - 1, g);
+                serp(x2n, y2n, x2, y2, n - 1, g);
+            }
+        }
+
+          
+        ////////////////////////////////////////////////////// КАНТОР
+          
+               private void drawButtonk_Click(object sender, EventArgs e)
+               {
+                   try
+                   {
+                       int size = resultPBk.ClientSize.Width / 2;
+                       int n = Convert.ToInt32(textBox1k.Text);
+                       var stack = new Stack<Rectangle>();
+                       Rectangle item = GetRectangle(resultPBk.Width / 2, resultPBk.Height / 2, size);
+                       stack.Push(item);
+
+                       var list = new List<Rectangle> { item };
+                       size /= 4;
+                       for (int i = 1; i < n && size > 1; i++, size /= 4)
+                       {
+                           var temp = new List<Rectangle>();
+                           foreach (Rectangle rectangle in list)
+                           {
+                               temp.Add(GetRectangle(rectangle.Left, rectangle.Top, size));
+                               temp.Add(GetRectangle(rectangle.Right, rectangle.Top, size));
+                               temp.Add(GetRectangle(rectangle.Left, rectangle.Bottom, size));
+                               temp.Add(GetRectangle(rectangle.Right, rectangle.Bottom, size));
+                           }
+                           foreach (Rectangle rectangle in temp)
+                           {
+                               stack.Push(rectangle);
+                           }
+                           list = temp;
+                       }
+
+                       var bmp = new Bitmap(resultPBk.Width, resultPBk.Height);
+
+                       var pen = new SolidBrush(resultPBk.BackColor);
+                       using (var g = Graphics.FromImage(bmp))
+                       {
+                           foreach (Rectangle rectangle in stack)
+                           {
+                               g.FillRectangle(pen, rectangle);
+                               g.DrawRectangle(Pens.BlueViolet, rectangle);
+                           }
+                           g.Dispose();
+                       }
+                       if (resultPBk.Image != null)
+                           resultPBk.Image.Dispose();
+                       resultPBk.Image = bmp;
+                   }
+                   catch (Exception ex)
+                   { MessageBox.Show(ex.Message); }
+               }
+               private Rectangle GetRectangle(int centerX, int centerY, int right)
+               {
+                   var rectangle = new Rectangle(centerX - right / 2, centerY - right / 2, right, right);
+                   return rectangle;
+               }
+        ////////////////////////////////// КОХ
+               static int Fractal(PointF p1, PointF p2, PointF p3, int n, Graphics gr)
+               {
+
+                   if (n > 0)
+                   {
+
+                       var p4 = new PointF((p2.X + 2 * p1.X) / 3, (p2.Y + 2 * p1.Y) / 3);
+                       var p5 = new PointF((2 * p2.X + p1.X) / 3, (p1.Y + 2 * p2.Y) / 3);
+                       var ps = new PointF((p2.X + p1.X) / 2, (p2.Y + p1.Y) / 2);
+                       var pn = new PointF((4 * ps.X - p3.X) / 3, (4 * ps.Y - p3.Y) / 3);
+
+                       gr.DrawLine(pen1, p4, pn);
+                       gr.DrawLine(pen1, p5, pn);
+                       gr.DrawLine(pen2, p4, p5);
+
+                       Fractal(p4, pn, p5, n - 1, gr);
+                       Fractal(pn, p5, p4, n - 1, gr);
+                       Fractal(p1, p4, new PointF((2 * p1.X + p3.X) / 3, (2 * p1.Y + p3.Y) / 3), n - 1, gr);
+                       Fractal(p5, p2, new PointF((2 * p2.X + p3.X) / 3, (2 * p2.Y + p3.Y) / 3), n - 1, gr);
+
+                   }
+                   return n;
+               }
+               private void button1h_Click(object sender, EventArgs e)
+               {
+                   try
+                   {
+                       n = Convert.ToInt32(textBox1h.Text);
+                       pen1 = new Pen(Color.Black, 1.5F);
+                       pen2 = new Pen(SystemColors.Control, 1.6F);
+                       //  pen2 = new Pen(Color.Black, 1.4F);
+
+                       m_Bm2 = new Bitmap(pictureBox1h.ClientSize.Width, pictureBox1h.ClientSize.Height);
+                       pictureBox1h.Image = m_Bm2;
+
+                       // gr = CreateGraphics();
+                       var p1 = new PointF(50, 70);
+                       var p2 = new PointF(350, 70);
+                       var p3 = new PointF(200, 270);
+                       /*   var p1 = new PointF(100, 100);
+                          var p2 = new PointF(400, 100);
+                          var p3 = new PointF(250, 300);*/
+                       using (Graphics gr = Graphics.FromImage(m_Bm2))
+                       {
+                           gr.Clear(pictureBox1h.BackColor);
+                           gr.DrawLine(pen1, p1, p2);
+                           gr.DrawLine(pen1, p2, p3);
+                           gr.DrawLine(pen1, p3, p1);
+                           Fractal(p1, p2, p3, n, gr);
+                           Fractal(p2, p3, p1, n, gr);
+                           Fractal(p3, p1, p2, n, gr);
+
+                           gr.Dispose();
+                       }
+                   }
+                   catch (Exception ex)
+
+                   { MessageBox.Show(ex.Message.ToString()); }
+
+               }
+        }
+    }
+
